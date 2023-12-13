b2a4519c8e2d364c0caf4316e64c328e84a8014e
Gabriel V. Santana <gabrielsantana.it@gmail.com>
2023-12-13 13:13:48 -0300

    ci: fix build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 import './globals.css';
 import type { Metadata } from 'next';
 
-export const fontSans = FontSans({
+const fontSans = FontSans({
   subsets: ['latin'],
   variable: '--font-sans',
 });
